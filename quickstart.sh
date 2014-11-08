977e62afbedf0bbe0399d5ad95516a197d79a9f5
Raphael Zimmermann <raphi.zimmermann@gmail.com>
2014-11-08 22:33:03 +0100

    Download quickstart files from master branch

--- quickstart.sh
+++ quickstart.sh
@@ -1,39 +1,32 @@
 #!/usr/bin/env bash
 
-# test if dir is empty...
+# Check if a target dir is given. If not, use cwd.
 if [[ -z "$1" ]]; then
   TARGET="./"
 else
   TARGET=$1
 fi
+
+# If the current directory is not empty, abort.
 if [ "$(ls -A $TARGET)" ]; then
      echo "The target directory ($TARGET) is not empty!"
      exit 1
 fi
 
-# Create directories
+# Create quickstart directories
 mkdir -p "$TARGET/protofast"
 mkdir -p "$TARGET/templates"
 mkdir -p "$TARGET/js"
 mkdir -p "$TARGET/css"
 
 
-# download the latest stabile
-#curl -o protofast.php https://github.com/raphiz/protofast/...
+# download the latest stabile quickstart files
 echo "Downloading...."
-cp protofast.php "$TARGET/protofast/"
+curl -o "$TARGET/protofast/protofast.php" https://raw.githubusercontent.com/raphiz/protofast/master/protofast.php
+curl -o "$TARGET/protofast.ini" https://raw.githubusercontent.com/raphiz/protofast/master/quickstart/protofast.ini
+curl -o "$TARGET/index.php" https://raw.githubusercontent.com/raphiz/protofast/master/quickstart/index.php
+curl -o "$TARGET/templates/base.html" https://raw.githubusercontent.com/raphiz/protofast/master/quickstart/templates/base.html
+curl -o "$TARGET/templates/index.html" https://raw.githubusercontent.com/raphiz/protofast/master/quickstart/templates/index.html
 
-# Create empty protofast.ini
-cp quickstart/protofast.ini "$TARGET"
-
-# create example index.php
-cp quickstart/index.php "$TARGET"
-
-# create example base.html
-cp quickstart/templates/base.html "$TARGET/templates/"
-
-# create example index.html
-cp quickstart/templates/index.html "$TARGET/templates/"
-
-
+# Print status message
 echo "Done! You can now run \"php -S localhost:8000\" in the target directory to get started!"
